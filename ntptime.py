6bd0f51b45e10ecc8c00f10ff4ed1e87f53c1c51
Mauro Hernan Riva <Mauro Hernan Riva>
2017-10-28 12:58:35 +0200

    initial commit

---
+++ ntptime.py
@@ -0,0 +1,51 @@
+#Copyright [2017] [Mauro Riva <lemariva@mail.com> <lemariva.com>]
+
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+
+#http://www.apache.org/licenses/LICENSE-2.0
+
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+
+#The above copyright notice and this permission notice shall be
+#included in all copies or substantial portions of the Software.
+
+try:
+    import usocket as socket
+except:
+    import socket
+try:
+    import ustruct as struct
+except:
+    import struct
+
+# (date(2000, 1, 1) - date(1900, 1, 1)).days * 24*60*60
+NTP_DELTA = 3155673600
+host = "time1.google.com"
+
+def time():
+    NTP_QUERY = bytearray(48)
+    NTP_QUERY[0] = 0x1b
+    addr = socket.getaddrinfo(host, 123)[0][-1]
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(1)
+    res = s.sendto(NTP_QUERY, addr)
+    msg = s.recv(48)
+    s = None
+    #s.close()
+    val = struct.unpack("!I", msg[40:44])[0]
+    return val - NTP_DELTA
+
+def settime():
+    t = time()
+    import machine
+    import utime
+    tm = utime.localtime(t)
+    tm = tm[0:3] + (0,) + tm[3:6] + (0,)
+    machine.RTC().datetime(tm)
+    print(utime.localtime())
---
+++ timeutils.py
@@ -0,0 +1,124 @@
+#Copyright [2017] [Mauro Riva <lemariva@mail.com> <lemariva.com>]
+
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+
+#http://www.apache.org/licenses/LICENSE-2.0
+
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+
+#The above copyright notice and this permission notice shall be
+#included in all copies or substantial portions of the Software.
+
+import time
+import math
+
+try:
+    import usocket as socket
+except:
+    import socket
+try:
+    import ustruct as struct
+except:
+    import struct
+
+NTP_PACKET_FORMAT = "!12I"
+NTP_DELTA = 3155673600  # 1970-01-01 00:00:00
+
+DaysPer4Years = 365*4+1
+EPOCH_DOW = 3
+SECS_PER_DAY = 60*60*24
+
+host = "time1.google.com"
+global_epoch = 0
+
+
+days = [[   0,  31,  60,  91, 121, 152, 182, 213, 244, 274, 305, 335],
+        [ 366, 397, 425, 456, 486, 517, 547, 578, 609, 639, 670, 700],
+        [ 731, 762, 790, 821, 851, 882, 912, 943, 974,1004,1035,1065],
+        [1096,1127,1155,1186,1216,1247,1277,1308,1339,1369,1400,1430],
+       ]
+
+def epoch():
+    global global_epoch
+    try:
+        NTP_QUERY = bytearray(48)
+        NTP_QUERY[0] = 0x1b
+        addr = socket.getaddrinfo(host, 123)[0][-1]
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.settimeout(1)
+        res = s.sendto(NTP_QUERY, addr)
+        msg = s.recv(48)
+        del(s) #s.close()
+        unpacked = struct.unpack(NTP_PACKET_FORMAT,
+                           msg[0:struct.calcsize(NTP_PACKET_FORMAT)])
+        global_epoch = (unpacked[10] + float(unpacked[11]) / 2**32 - NTP_DELTA)
+    except Exception as e:
+        print("Something wrong with the ntp-server: " + str(e))
+    finally:
+        epoch = global_epoch
+    return epoch
+
+# year 2000-2099, no DST, no leap second, no timezone.
+# epoch = (((year/4*(365*4+1)+days[year%4][month]+day)*24+hour)*60+minute)*60+second;
+def gmtime(epoch):
+
+    epoch_tmp = epoch
+    seconds = int(epoch%60)
+    epoch = epoch / 60
+    minutes = int(epoch%60)
+    epoch = epoch / 60
+    hours = int(epoch%24)
+    epoch = epoch / 24
+
+    years = epoch/(DaysPer4Years)*4
+    epoch = epoch%(DaysPer4Years)
+
+    for year in range(2,-1,-1):
+        if (epoch >= days[year][0]):
+            break
+    for month in range(11,-1,-1):
+        if (epoch >= days[year][month]):
+            break
+
+    day = int(epoch - days[year][month]) + 1
+    year = 2000 + int(years) + year - 1
+    month = month + 1
+
+    dow = math.ceil((epoch_tmp / (SECS_PER_DAY)) % 7 - EPOCH_DOW)
+
+    #print("epoch:" + str(epoch_tmp) + "-> year: " + str(year) + " month: " + str(month) + " day: " + str(day) + " hour: " + str(hours) + " minutes: " + str(minutes) + " dow: " + str(dow))
+
+    return (year, month, day, hours, minutes, seconds, dow)
+
+def formatdate(time_epoch = None):
+    """Returns a date string as specified by RFC 2822, e.g.:
+    Fri, 09 Nov 2001 01:08:47 GMT
+    """
+    if(time_epoch == None):
+        time_epoch = epoch()
+    now = gmtime(time_epoch)
+    zone = 'GMT'
+
+    try:
+        date_str = _format_timetuple_and_zone(now, zone)
+    except Exception as e:
+        date_str = "none"
+        print("Timeutils exception: " + str(e))
+
+    return date_str
+
+
+def _format_timetuple_and_zone(timetuple, zone):
+    return '%s, %02d %s %04d %02d:%02d:%02d %s' % (
+        ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'][timetuple[6]],
+        timetuple[2],
+        ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+         'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'][timetuple[1] - 1],
+        timetuple[0], timetuple[3], timetuple[4], timetuple[5],
+        zone)
